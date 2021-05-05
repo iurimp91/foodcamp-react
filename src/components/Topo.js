8079914fb9a2f7c659a49b7af0ab4a9b247c1600
Iuri Magnago <iurimp91@gmail.com>
2021-05-04 22:11:30 -0300

    Implementado layout de uma opção

--- src/components/Topo.js
+++ src/components/Topo.js
@@ -1,10 +1,8 @@
 export default function Topo() {
     return (
-        <>
-            <header>
-                <h1>FoodCamp</h1>
-                <span>Sua comida em 6 minutos</span>
-            </header>
-        </>
+        <header>
+            <h1>FoodCamp</h1>
+            <span>Sua comida em 6 minutos</span>
+        </header>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,27 @@ import Topo from "./components/Topo";
 
 function App() {
     return (
-        <Topo />
+        <>
+            <Topo />
+            <div className="conteudo">
+                <h1 className="titulo-menu">Primeiro, seu prato</h1>
+                <ul className="menu-opcoes">
+                    <li className="opcao">
+                        <img src="https://http2.mlstatic.com/D_NQ_NP_620438-MLB29512328329_022019-O.jpg" alt="frango" />
+                        <h1 className="titulo-opcao">Frango Yin Yang</h1>
+                        <span className="texto-opcao">Um pouco de batata, um pouco de salada</span>
+                        <div className="preco-contador">
+                            <span className="preco-opcao">R$ 14,90</span>
+                            <span>
+                                <button className="subtrair">-</button>
+                                1
+                                <button className="adicionar">+</button>
+                            </span>
+                        </div>
+                    </li>
+                </ul> 
+            </div>
+        </>
     );
 }
 
