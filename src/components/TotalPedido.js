9cd3fcf5ee9913fb00b096b6a473d84ee57d6bb0
thiagoribb <thiribeiro142@gmail.com>
2021-05-11 18:04:38 -0300

    Preparando review

--- src/components/TotalPedido.js
+++
@@ -1,8 +0,0 @@
-export default function TotalPedido(props) {
-    return (
-        <li className="total-pedido">
-            <span>TOTAL</span>
-            <span>R$ {props.total.toFixed(2).replace(".", ",")}</span>
-        </li>
-    );
-}
